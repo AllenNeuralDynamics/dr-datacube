ab169412bad22947c2562ba49f6a3f1e2363c8a2
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2026-09-02 11:06:55 -0700

    Format

--- scripts/dump_datacube_sessions.py
+++ scripts/dump_datacube_sessions.py
@@ -23,9 +23,9 @@ def session_table() -> pl.DataFrame:
         good = datacube.get_sessions(session_type).with_columns(pl.lit(True).alias("is_behavior_pass"))
         all = datacube.get_sessions(session_type, with_behavior_filter=False)
         dfs.extend([df.with_columns(session_type=pl.lit(session_type)) for df in (good, all)])
-    df= (
-        pl.concat(dfs, how='diagonal')
-        .drop('keywords')
+    df = (
+        pl.concat(dfs, how="diagonal")
+        .drop("keywords")
         .with_columns(
             pl.col("is_behavior_pass").fill_null(pl.lit(False)),
         )
@@ -35,10 +35,12 @@ def session_table() -> pl.DataFrame:
     assert df["is_behavior_pass"].null_count() == 0
     return df
 
+
 def dump_session_table() -> None:
     output_dir = Path(__file__).resolve().parents[1] / "assets"
     output_dir.mkdir(parents=True, exist_ok=True)
     session_table().write_csv(output_dir / "datacube_sessions.csv")
+
 
 if __name__ == "__main__":
     dump_session_table()
--- src/dr_datacube/datacube.py
+++ src/dr_datacube/datacube.py
@@ -13,11 +13,14 @@ from polars._typing import FrameType
 
 logger = logging.getLogger(__name__)
 
+
 def pipeline_data_dir() -> upath.UPath:
     return upath.UPath("/tmp/data")
 
+
 def capsule_data_dir() -> upath.UPath:
     return upath.UPath("/root/capsule/data")
+
 
 @functools.cache
 def on_codeocean() -> bool:
@@ -25,11 +28,13 @@ def on_codeocean() -> bool:
         return capsule_data_dir().exists() or is_pipeline()
     return False
 
+
 @functools.cache
 def is_pipeline() -> bool:
     with contextlib.suppress(Exception):
         return pipeline_data_dir().exists() and bool(os.environ.get("AWS_BATCH_JOB_ID"))
     return False
+
 
 class DatacubeConfig(pydantic_settings.BaseSettings):
     model_config = pydantic.ConfigDict(validate_assignment=True)
@@ -37,11 +42,11 @@ class DatacubeConfig(pydantic_settings.BaseSettings):
     version: str = "v0.0.289"
     use_cache: bool = False
     s3_cache_dir: upath.UPath = upath.UPath("s3://aind-scratch-data/dynamic-routing/cache", anon=True)
-    
+
     @property
     def asset_dir(self) -> upath.UPath:
         if on_codeocean():
-            data_dir = pipeline_data_dir() if is_pipeline() else capsule_data_dir() 
+            data_dir = pipeline_data_dir() if is_pipeline() else capsule_data_dir()
             try:
                 datacube_dir = tuple(data_dir.glob("dynamicrouting_datacube*"))
             except StopIteration:
@@ -50,22 +55,28 @@ class DatacubeConfig(pydantic_settings.BaseSettings):
                 raise FileNotFoundError(f"Could not find dynamicrouting_datacube data asset in {data_dir}")
             if len(datacube_dir) > 1:
                 choice = next((d for d in datacube_dir if self.version in d.name), datacube_dir[0])
-                logger.warning(f"Found multiple dynamicrouting_datacube data assets in {data_dir}, using: {choice} (set `datacube_config.version` to change)")
+                logger.warning(
+                    f"Found multiple dynamicrouting_datacube data assets in {data_dir}, using: {choice} (set `datacube_config.version` to change)"
+                )
                 return choice
             return datacube_dir[0]
         # get S3 dir of datacube asset from CO API
         try:
             import aind_session
         except ImportError:
-            raise ImportError("aind_session and a CO_API_TOKEN are required to find the datacube data asset on S3. Install as an optional-dependency with `dr-datacube[co]`.")
+            raise ImportError(
+                "aind_session and a CO_API_TOKEN are required to find the datacube data asset on S3. Install as an optional-dependency with `dr-datacube[co]`."
+            )
         return aind_session.get_data_asset_source_dir(
-            next(d for d in reversed(aind_session.get_data_assets('dynamicrouting_datacube')) if self.version in d.name).id
+            next(
+                d for d in reversed(aind_session.get_data_assets("dynamicrouting_datacube")) if self.version in d.name
+            ).id
         )
 
     @property
     def nwb_dir(self) -> upath.UPath:
         if self.use_cache:
-            return self.s3_cache_dir / "nwb" / self.version 
+            return self.s3_cache_dir / "nwb" / self.version
         return self.asset_dir / "nwb"
 
     @property
@@ -74,8 +85,10 @@ class DatacubeConfig(pydantic_settings.BaseSettings):
             return self.s3_cache_dir / "nwb_components" / self.version / "consolidated"
         else:
             return self.nwb_dir.parent / "consolidated"
-            
+
+
 datacube_config = DatacubeConfig()
+
 
 def get_lf(name: str, use_cache: bool | None = None) -> pl.LazyFrame:
     if use_cache is not None:
@@ -85,6 +98,7 @@ def get_lf(name: str, use_cache: bool | None = None) -> pl.LazyFrame:
     storage_options = {} if not config.use_cache else {"skip_signature": "true", "region": "us-west-2"}
     return pl.scan_parquet((config.parquet_dir / f"{name}.parquet").as_posix(), storage_options=storage_options)
 
+
 @functools.cache
 def list_nwb_sources() -> tuple[str, ...]:
     """Get all file URIs."""
@@ -92,8 +106,9 @@ def list_nwb_sources() -> tuple[str, ...]:
     logger.info(f"Found {len(sources)} NWB sources in {datacube_config.nwb_dir}")
     return tuple(sources)
 
+
 def ensure_id_cols(df: FrameType) -> FrameType:
-    schema = df.lazy().collect_schema() # works if we pass a dataframe or lazyframe
+    schema = df.lazy().collect_schema()  # works if we pass a dataframe or lazyframe
     if "session_id" in schema and "subject_id" in schema:
         logger.debug("DataFrame already has a `session_id` and `subject_id` columns, skipping parsing from `_nwb_path`")
         return df
@@ -107,6 +122,7 @@ def ensure_id_cols(df: FrameType) -> FrameType:
         df = df.with_columns(pl.col("session_id").str.split("_").list.get(0).alias("subject_id"))
     return df
 
+
 def behavior_summary(block_dprime_threshold: float = 1.0) -> pl.DataFrame:
     return (
         get_lf("performance")
@@ -115,26 +131,39 @@ def behavior_summary(block_dprime_threshold: float = 1.0) -> pl.DataFrame:
             pl.col("cross_modality_dprime").ge(block_dprime_threshold).alias("is_good_block"),
         )
         .group_by("session_id", "rewarded_modality")
-        .agg(
-            pl.col("is_engaged_block", "is_good_block").sum()
-        )
+        .agg(pl.col("is_engaged_block", "is_good_block").sum())
         .group_by("session_id")
         .agg(
             pl.col("is_good_block").filter(pl.col("rewarded_modality") == "vis").first().alias("n_good_vis_blocks"),
             pl.col("is_good_block").filter(pl.col("rewarded_modality") == "aud").first().alias("n_good_aud_blocks"),
-            pl.col("is_good_block").filter("is_engaged_block", pl.col("rewarded_modality") == "vis").first().alias("n_good_engaged_vis_blocks"),
-            pl.col("is_good_block").filter("is_engaged_block", pl.col("rewarded_modality") == "aud").first().alias("n_good_engaged_aud_blocks"),
+            pl.col("is_good_block")
+            .filter("is_engaged_block", pl.col("rewarded_modality") == "vis")
+            .first()
+            .alias("n_good_engaged_vis_blocks"),
+            pl.col("is_good_block")
+            .filter("is_engaged_block", pl.col("rewarded_modality") == "aud")
+            .first()
+            .alias("n_good_engaged_aud_blocks"),
             pl.col("is_engaged_block").sum().alias("n_engaged_blocks"),
         )
-    ).collect()   # Added return statement
+    ).collect()  # Added return statement
+
 
 def brainwide_ephys_filter(with_behavior_filter: bool = True) -> pl.Expr:
-    required = ("prod", "brainwide_survey", "task", "ephys", "ccf") # good_behavior is incorrect - will be fixed in v0.0.290
-    excluded = ("issues", "context naive") # context_naive (w underscore) had bug - should be mutually exclusive with bws from >=v0.0.290
+    required = (
+        "prod",
+        "brainwide_survey",
+        "task",
+        "ephys",
+        "ccf",
+    )  # good_behavior is incorrect - will be fixed in v0.0.290
+    excluded = (
+        "issues",
+        "context naive",
+    )  # context_naive (w underscore) had bug - should be mutually exclusive with bws from >=v0.0.290
     if with_behavior_filter:
         good_behavior_session_ids = (
-            behavior_summary(block_dprime_threshold=1.0)
-            .filter(
+            behavior_summary(block_dprime_threshold=1.0).filter(
                 pl.col("n_good_engaged_aud_blocks").ge(2),
                 pl.col("n_good_engaged_vis_blocks").ge(2),
             )
@@ -144,18 +173,18 @@ def brainwide_ephys_filter(with_behavior_filter: bool = True) -> pl.Expr:
     return pl.all_horizontal(
         *[pl.col("keywords").list.contains(keyword) for keyword in required],
         *[~pl.col("keywords").list.contains(keyword) for keyword in excluded],
-        pl.col("session_id").is_in(good_behavior_session_ids) if with_behavior_filter else pl.lit(True)
+        pl.col("session_id").is_in(good_behavior_session_ids) if with_behavior_filter else pl.lit(True),
     )
 
+
 def naive_ephys_filter(with_behavior_filter: bool = True) -> pl.Expr:
-    required = ("dynamic_routing", "task", "ephys", "ccf", "context naive") 
+    required = ("dynamic_routing", "task", "ephys", "ccf", "context naive")
     # TODO prod should be included, but is incorrect
-    #TODO switch to "context_naive" (w/underscore) when fixed in v0.0.290
-    excluded = ("issues", "templeton") # TODO add "brainwide_survey" when fixed in v0.0.290
+    # TODO switch to "context_naive" (w/underscore) when fixed in v0.0.290
+    excluded = ("issues", "templeton")  # TODO add "brainwide_survey" when fixed in v0.0.290
     if with_behavior_filter:
         engaged_session_ids = (
-            behavior_summary()
-            .filter(
+            behavior_summary().filter(
                 pl.col("n_engaged_blocks").ge(4),
             )
         )["session_id"].to_list()
@@ -166,18 +195,22 @@ def naive_ephys_filter(with_behavior_filter: bool = True) -> pl.Expr:
         *[~pl.col("keywords").list.contains(keyword) for keyword in excluded],
         pl.col("session_id").is_in(engaged_session_ids) if with_behavior_filter else pl.lit(True),
     )
-    
+
+
 def templeton_ephys_filter(with_behavior_filter: bool = True) -> pl.Expr:
     required = ("prod", "templeton", "task", "ephys", "ccf")
-    excluded = ("issues", ) 
+    excluded = ("issues",)
     if with_behavior_filter:
         good_behavior_session_ids = (
-            get_lf("performance")
-            .filter(
-                pl.col('cross_modality_dprime').is_null(),
-                pl.col('aud_dprime').ge(1.0) | pl.col('vis_dprime').ge(1.0),
+            (
+                get_lf("performance").filter(
+                    pl.col("cross_modality_dprime").is_null(),
+                    pl.col("aud_dprime").ge(1.0) | pl.col("vis_dprime").ge(1.0),
+                )
             )
-        ).collect()["session_id"].to_list()
+            .collect()["session_id"]
+            .to_list()
+        )
     else:
         good_behavior_session_ids = []
     return pl.all_horizontal(
@@ -186,23 +219,25 @@ def templeton_ephys_filter(with_behavior_filter: bool = True) -> pl.Expr:
         pl.col("session_id").is_in(good_behavior_session_ids) if with_behavior_filter else pl.lit(True),
     )
 
+
 def filter_functions() -> dict[str, Callable[[bool], pl.Expr]]:
     """Return a dict of filter functions for each session_type. Each function has signature (with_behavior_filter: bool = True) -> pl.Expr."""
     return {
-        'brainwide': brainwide_ephys_filter, 
-        'naive': naive_ephys_filter, 
-        'templeton': templeton_ephys_filter,
+        "brainwide": brainwide_ephys_filter,
+        "naive": naive_ephys_filter,
+        "templeton": templeton_ephys_filter,
     }
+
 
 @functools.cache
 def get_sessions(
-    session_type: Literal['brainwide', 'naive', 'templeton'] | None = 'brainwide',
+    session_type: Literal["brainwide", "naive", "templeton"] | None = "brainwide",
     with_behavior_filter: bool = True,
     only_in_data_asset: bool = True,
     filter_expr: pl.Expr | None = None,
 ) -> pl.DataFrame:
     """A DataFrame with 'session_id' and 'keywords'.
- 
+
     Options:
     'brainwide' (default) - standard brainwide survey ephys sessions
     'naive' - context naive dynamic routing ephys sessions.
@@ -225,17 +260,24 @@ def get_sessions(
         get_lf("session")
         .pipe(ensure_id_cols)
         .filter(filter_expr)
-        .select('session_id', 'subject_id', 'keywords')
+        .select("session_id", "subject_id", "keywords")
         .collect()
     )
     if only_in_data_asset:
-        session_ids_in_data_asset = pl.read_parquet((datacube_config.asset_dir / 'session_table.parquet').as_posix(), columns=["session_id"])["session_id"].sort().to_list()
+        session_ids_in_data_asset = (
+            pl.read_parquet((datacube_config.asset_dir / "session_table.parquet").as_posix(), columns=["session_id"])[
+                "session_id"
+            ]
+            .sort()
+            .to_list()
+        )
         filtered = filtered.filter(pl.col("session_id").is_in(session_ids_in_data_asset))
     return filtered
 
+
 def get_session_ids_from_github(
-    session_type: Literal['brainwide', 'naive', 'templeton'] | None = 'brainwide', 
-    with_behavior_filter: bool = True, 
+    session_type: Literal["brainwide", "naive", "templeton"] | None = "brainwide",
+    with_behavior_filter: bool = True,
 ) -> list[str]:
     """Return a list of session IDs for the given session type, without requiring credentials to access the CO data asset."""
     if session_type is None:
@@ -244,10 +286,17 @@ def get_session_ids_from_github(
         filter_expr = pl.col("session_type") == session_type
     if with_behavior_filter:
         filter_expr = filter_expr & pl.col("is_behavior_pass")
-    return pl.read_csv(
-        "https://raw.githubusercontent.com/AllenNeuralDynamics/dr-datacube/main/assets/datacube_sessions.csv"
-    ).filter(filter_expr)["session_id"].sort().to_list()
+    return (
+        pl.read_csv(
+            "https://raw.githubusercontent.com/AllenNeuralDynamics/dr-datacube/main/assets/datacube_sessions.csv"
+        )
+        .filter(filter_expr)["session_id"]
+        .sort()
+        .to_list()
+    )
+
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
